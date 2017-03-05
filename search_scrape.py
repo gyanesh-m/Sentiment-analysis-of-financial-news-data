0317f33bf621266e08a5bf0b026c278989ff542c
Yoman <abc@def.com>
2017-03-05 13:29:58 +0530

    fixed errors

--- search_scrape.py
+++ search_scrape.py
@@ -11,14 +11,13 @@ import random,os
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 def writeToFile(links,company,date,month,year):
 	try:
-		os.makedirs('data')
+		os.makedirs('links')
 	except Exception as e:
 		pass
 	
-	f = open(os.path.join(BASE_PATH,"data","results_"+comp+"_"+sd+"_"+sm+"_"+sy+'.data'),'a+')
-	for i in links:
-		f.write(str(i)+"\n")
-
+	f = open(os.path.join(BASE_PATH,"links","results_"+company+"_"+date+"_"+month+"_"+year+'.data'),'a+')
+	for i,j in links:
+		f.write(str(i)+"::"+str(j)+"\n")
 	f.close()
 driver_path = ''
 
@@ -41,6 +40,7 @@ driver.implicitly_wait(15)
 start_url="https://www.google.co.in/search?num=100&biw=1040&bih=635&tbs=cdr%3A1%2Ccd_min%3A"+"{smonth}"+r"%2F"+"{sday}"+r"%2F"+"{syear}"+r"%2Ccd_max%3A"+"{emonth}"+r"%2F"+"{eday}"+r"%2F"+"{eyear}"+r"%2Csbd%3A1&tbm=nws&q=site%3A"+"{webpage}+%22{company}%22&&oq=site%3A{webpage}"+"%22{company}%22&gs_l=serp.3...0.0.0.1565.0.0.0.0.0.0.0.0..0.0....0...1c..64.serp..0.0.0.IfKqhHBbtbY"
 driver.get(start_url.format(smonth=sm,sday=sd,syear=sy,emonth=em,eday=ed,eyear=ey,webpage=webp,company=comp))
 data={}
+date=[]
 next_page=""
 wait = WebDriverWait(driver, random.randint(10,20))
 i=0
@@ -48,20 +48,38 @@ time.sleep(random.randint(8,13))
 previous=next_page
 try:
 	while next_page is not None or previous is not None:
-			time.sleep(random.randrange(15,20))
+			if i >= 80:
+				break
+			time.sleep(random.randrange(20,30))
+			list_links=[]
+			dat = []
 			list_links=[]
 			try:
 				next_page = wait.until(EC.element_to_be_clickable((By.ID,'navcnt')))
 				next_page = driver.find_element_by_link_text('Next')
 				element=driver.find_elements_by_xpath("*//h3/a")
+
+				dat = driver.find_elements_by_xpath('//*[@id="rso"]/div/div/div/div/div[1]/span[3]')
 			except Exception as e:
 				print (e)
 				next_page=None
 				#this is for the last page when no next option will be available
 				element=driver.find_elements_by_xpath("*//h3/a")
+
+				dat = driver.find_elements_by_xpath('//*[@id="rso"]/div/div/div/div/div[1]/span[3]')
+				
 			for ele in element:
 				list_links.append(ele.get_attribute("href"))
+			
+			for d in dat:
+				date.append(d.text);
+
 			data[i]=list_links
+			for ele in element:
+				list_links.append(ele.get_attribute("href"))
+
+			list_links = zip(date,list_links)
+
 			writeToFile(list_links,comp,sd,sm,sy)
 			i+=1
 			if next_page is not None:
@@ -70,7 +88,8 @@ try:
 				break
 except Exception as e:
 	print(e)
-finally:	
+
+finally:
 	print("*"*8+"Finished fetching all the urls"+"*"*8)
 	count=0
 	for i in data.keys():
