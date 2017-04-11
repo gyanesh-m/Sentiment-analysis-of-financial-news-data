38fea965805d77fa603466c36b95595840c048de
gyanesh-m <gyaneshmalhotra@gmail.com>
2017-04-12 03:00:18 +0530

    add hindu-businessline scraper

--- scrape_with_bs4.py
+++ scrape_with_bs4.py
@@ -31,21 +31,19 @@ def make_directory(company):
 		os.makedirs('content/'+company)
 	except Exception as e:
 		pass	
+
+#All the content scrapers will return a list of strings for a specific website.
 def sc_reuters(bs):
-	data=[]
 	d=bs.find_all(id='article-text')
 	content=[i.get_text() for i in d]
 	n=content[-1].rfind("(")
 	content[-1]=content[-1][:n]
-	data.extend(content)
-	return data
+	return content
 
 def sc_thehindu(bs):
-	t=[]
 	temp=bs.select('div > p')
-	temp2=[j.get_text() for i,j in enumerate(temp) if i<len(temp)-3]
-	t.extend(temp2)
-	return t
+	content=[j.get_text() for i,j in enumerate(temp) if i<len(temp)-3]
+	return content
 
 def sc_econt(bs):
 	t=[]
@@ -62,9 +60,10 @@ def moneyControl(bs):
 			temp = i.find_all('p')
 		for i in temp:
 			t.append(i.get_text())
-
+	except Exception as e:
+		print(e)
 	return t
-
+	
 def ndtv(bs):
 	t=[]
 	#profit.ndtv not working :S
@@ -76,44 +75,54 @@ def ndtv(bs):
 	for i in y:
 		t.append(i.get_text())
 	return t
+def hindu_bl(bs):
+	t=[]
+	data=bs.find_all(class_='article-text')
+	for i in data:
+		y=i.find_all('p')
+	for i in y:
+		t.append(i.get_text())
+	return t
 
-NEWS={'reuters.com':sc_reuters,'thehindu.com':sc_thehindu,'economictimes.indiatimes':sc_econt,'moneycontrol.com':moneyControl,'ndtv.com':ndtv}
-for file in list_files('links/'):
-	print(file)
-	company = file.split('_')[2]
-	links = [line.rstrip('\n') for line in open('links/'+file)]
-	webp=file.split('_')[1]
-	print(webp)
-	b = {}
-	date = []
-	content = []
-	for url in links:
-		c,d= url.split('::')
-		if 'khabar.ndtv' in d:
-			continue
-		r = requests.get(d)
-		print("Scraping url ",d)
-		soup = BeautifulSoup(r.content,"html.parser")
+if __name__ == '__main__':
+	
+	NEWS={'reuters.com':sc_reuters,'thehindu.com':sc_thehindu,'economictimes.indiatimes':sc_econt,'moneycontrol.com':moneyControl,'ndtv.com':ndtv}
+	for file in list_files('links/'):
+		print(file)
+		company = file.split('_')[2]
+		links = [line.rstrip('\n') for line in open('links/'+file)]
+		webp=file.split('_')[1]
+		print(webp)
+		b = {}
+		date = []
+		content = []
+		for url in links:
+			c,d= url.split('::')
+			if 'khabar.ndtv' in d:
+				continue
+			r = requests.get(d)
+			print("Scraping url ",d)
+			soup = BeautifulSoup(r.content,"html.parser")
 
-		a=NEWS[webp](soup)
+			a=NEWS[webp](soup)
 
-		str1 = ''.join(a)
-		c = datetime.datetime.strptime(c, '%d-%b-%Y')
-		date.append(c)
-		content.append(str1)
-		temp = {c:str1}
-		b.update(temp)
+			str1 = ''.join(a)
+			c = datetime.datetime.strptime(c, '%d-%b-%Y')
+			date.append(c)
+			content.append(str1)
+			temp = {c:str1}
+			b.update(temp)
 
-	make_directory(company)
+		make_directory(company)
 
-	with open('content/'+company+'/raw_'+file.split('.data')[0]+'.pkl', 'wb') as fp:
-	    pickle.dump(b, fp)
+		with open('content/'+company+'/raw_'+file.split('.data')[0]+'.pkl', 'wb') as fp:
+		    pickle.dump(b, fp)
 
-	temp = {'date':date,
-			'data':content}
+		temp = {'date':date,
+				'data':content}
 
-	df = pd.DataFrame(temp)
-	df.set_index('date',inplace=True)
-	df.to_pickle('content/'+company+'/'+file.split('.data')[0]+'_content.pkl')
-	df.to_csv('content/'+company+'/'+file.split('.data')[0]+'_content.csv')
-	tracker(file)
+		df = pd.DataFrame(temp)
+		df.set_index('date',inplace=True)
+		df.to_pickle('content/'+company+'/'+file.split('.data')[0]+'_content.pkl')
+		df.to_csv('content/'+company+'/'+file.split('.data')[0]+'_content.csv')
+		tracker(file)
